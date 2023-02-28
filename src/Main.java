d896e431333a6c774aa9c99be799998278abb93b
teresakoetter <t.koetter@mailbox.org>
2023-02-28 17:34:04 +0100

    plus7neueFiles

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import com.sun.source.util.SourcePositions;
+
+import javax.sound.midi.Soundbank;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean length = lengthTest("dlfjgggggghkfdjh");
+        System.out.println(length);
+        boolean fancy = fancyCheck("schalafancyschala");
+        System.out.println(fancy);
+        //boolean cM = callM();
+        //System.out.println(cM);
+        callM();
+        //both("kdfjghfdkjghfjk");
+        System.out.println(both("akdjfgfanchfdkfkjh"));
+
+        }
+
+        public static boolean lengthTest (String word){
+        if(word.length() > 2){
+            return true;
+        }else{
+            return false;
+        }
+        }
+        public static boolean fancyCheck(String word){
+        if(word.contains("fancy")){
+            return true;
+        }else{
+            return false;
+        }
+
+        }
+        public static  void callM (){
+            System.out.println(lengthTest("akdfjghkf"));
+        }
+
+        public static boolean both(String word){
+        return lengthTest(word) && fancyCheck(word);
+        }
+
+        }
+
+
+
+
+
